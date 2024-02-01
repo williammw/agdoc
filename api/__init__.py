e34242b387c2a1ee6a494741ff8be155927eafdb
wwwwwmw <william.manwai@gmail.com>
2024-02-01 15:44:16 +0000

    Remove unnecessary files and update .gitignore

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from .v1.umami.menu import menu_blueprint
 from .v1.fortune import fortune_blueprint
 from .v1.fortune.menu import fortune_menu_blueprint
 from .v1.fortune.base import fortune_base_blueprint
+from .v1.asr.base import asr_blueprint
 
 
 def create_app():
@@ -15,11 +16,15 @@ def create_app():
     # Register the Menu blueprint as a sub-route of Umami
     app.register_blueprint(menu_blueprint, url_prefix='/api/v1/umami/menu')
 
+    # Register the ASR blueprint
+    app.register_blueprint(asr_blueprint, url_prefix='/api/v1/asr')
+
     # fortune
     app.register_blueprint(fortune_blueprint, url_prefix='/api/v1/fortune')
-    app.register_blueprint(
-        fortune_menu_blueprint, url_prefix='/api/v1/fortune/menu')
+    app.register_blueprint(fortune_menu_blueprint,
+                           url_prefix='/api/v1/fortune/menu')
 
     app.register_blueprint(
         fortune_base_blueprint, url_prefix='/api/v1/fortune/base')
+
     return app
---
+++ api/v1/asr/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+
+asr_blueprint = Blueprint('fortune', __name__)
