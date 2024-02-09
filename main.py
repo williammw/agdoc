9b22b1b5a569336bca2bd421b222e435483d46ea
wwwwwmw <william.manwai@gmail.com>
2024-02-09 14:41:28 +0000

    Add API endpoints for Umami and AGI

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routers import umami_router
+from routers import agi_router
+
 
 app = FastAPI()
 
 app.include_router(umami_router.router, prefix="/api/v1/umami")
+app.include_router(agi_router.router, prefix="/api/v1/agi")
+
