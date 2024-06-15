bb9eccb5835c5abc250d7dc2e5d97267b7c09f31
wwwwwmw <william.manwai@gmail.com>
2024-06-15 01:45:03 +0100

    chore: Add CDN functionality for uploading and listing objects

--- app/routers/cdn_router.py
+++ app/routers/cdn_router.py
@@ -1,7 +1,8 @@
 # cnd_router.py
-from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
+from fastapi import APIRouter, HTTPException, UploadFile, File, Depends, Header
 from databases import Database
 from app.dependencies import get_database
+from app.firebase_admin_config import verify_token
 import boto3
 import httpx
 import os
@@ -17,9 +18,37 @@ r2_client = boto3.client(
     region_name='weur'
 )
 
+bucket_name = 'umami'
+allowed_mime_types = ['image/jpeg', 'image/png',
+                      'image/gif', 'video/mp4', 'video/mpeg']
+
+
+@router.get("/list-objects/")
+async def list_objects(database: Database = Depends(get_database), authorization: str = Header(...)):
+    token = authorization.split(" ")[1]
+    decoded_token = verify_token(token)
+    user_id = decoded_token['uid']
+
+    try:
+        # Query the database for objects belonging to the user
+        query = "SELECT filename, url, content_type, r2_object_key FROM cloudflare_r2_data WHERE user_id = :user_id"
+        values = {"user_id": user_id}
+        objects = await database.fetch_all(query=query, values=values)
+
+        if objects:
+            return {"objects": objects}
+        else:
+            return {"message": "No objects found for this user."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.post("/upload-image-cloudflare/")
-async def upload_to_cloudflare(file: UploadFile = File(...), database: Database = Depends(get_database)):
+async def upload_to_cloudflare(file: UploadFile = File(...), database: Database = Depends(get_database), authorization: str = Header(...)):
+    token = authorization.split(" ")[1]
+    decoded_token = verify_token(token)
+    user_id = decoded_token['uid']
+
     headers = {
         "Authorization": f"Bearer {os.getenv('CLOUDFLARE_STREAM_AND_IMAGES_API_TOKEN')}",
     }
@@ -42,14 +71,15 @@ async def upload_to_cloudflare(file: UploadFile = File(...), database: Database 
             status_code=500, detail="URL not returned from Cloudflare")
 
     query = """
-    INSERT INTO cloudflare_images (filename, url, content_type, cloudflare_id)
-    VALUES (:filename, :url, :content_type, :cloudflare_id)
+    INSERT INTO cloudflare_images (filename, url, content_type, cloudflare_id, user_id)
+    VALUES (:filename, :url, :content_type, :cloudflare_id, :user_id)
     """
     values = {
         "filename": file.filename,
         "url": url,
         "content_type": file.content_type,
         "cloudflare_id": image_data.get("result", {}).get("id"),
+        "user_id": user_id,
     }
 
     await database.execute(query=query, values=values)
@@ -58,12 +88,18 @@ async def upload_to_cloudflare(file: UploadFile = File(...), database: Database 
 
 
 @router.post("/upload-image-r2/")
-async def upload_to_r2(file: UploadFile = File(...), database: Database = Depends(get_database)):
-    bucket_name = os.getenv('R2_BUCKET_NAME')
+async def upload_to_r2(file: UploadFile = File(...), database: Database = Depends(get_database), authorization: str = Header(...)):
+    token = authorization.split(" ")[1]
+    decoded_token = verify_token(token)
+    user_id = decoded_token['uid']
+
+    # Validate file type
+    mime_type = file.content_type
+    if mime_type not in allowed_mime_types:
+        raise HTTPException(status_code=400, detail="Unsupported file type")
 
     # Generate a unique file name to avoid conflicts
-    # Ensure this is unique if necessary, e.g., by appending a timestamp or UUID
-    file_key = f"images/{file.filename}"
+    file_key = f"{user_id}/images/{file.filename}"
 
     try:
         # Upload file to R2
@@ -71,7 +107,7 @@ async def upload_to_r2(file: UploadFile = File(...), database: Database = Depend
             file.file,
             bucket_name,
             file_key,
-            ExtraArgs={"ContentType": file.content_type}
+            ExtraArgs={"ContentType": file.content_type, "ACL": "public-read"}
         )
     except Exception as e:
         raise HTTPException(
@@ -80,18 +116,19 @@ async def upload_to_r2(file: UploadFile = File(...), database: Database = Depend
         )
 
     # Construct the file URL
-    file_url = f"{os.getenv('R2_ENDPOINT_URL')}/{bucket_name}/{file_key}"
+    file_url = f"https://{os.getenv('R2_DEV_URL')}/{file_key.replace('/', '%2F')}"
 
     # Proceed with database insertion
     query = """
-    INSERT INTO cloudflare_r2_data (filename, url, content_type, r2_object_key)
-    VALUES (:filename, :url, :content_type, :r2_object_key)
+    INSERT INTO cloudflare_r2_data (filename, url, content_type, r2_object_key, user_id)
+    VALUES (:filename, :url, :content_type, :r2_object_key, :user_id)
     """
     values = {
         "filename": file.filename,
         "url": file_url,
         "content_type": file.content_type,
-        "r2_object_key": file_key  # Ensure this is correctly passed
+        "r2_object_key": file_key,
+        "user_id": user_id
     }
 
     try:
