108cd90d9e2f2fdf768b3dfb01708698d9c9e768
wwwwwmw <william.manwai@gmail.com>
2024-07-05 16:06:55 +0100

    chore: Update CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -21,10 +21,10 @@ logger = logging.getLogger(__name__)
 
 # CORS configuration
 origins = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://192.168.1.2:5173",
-    "http://192.168.1.2:8000",
+    # "http://localhost:5173",
+    # "http://localhost:8000",
+    # "http://192.168.1.2:5173",
+    # "http://192.168.1.2:8000",
     "https://235534.netlify.app",
     "https://umamiverse.netlify.app"
 ]
