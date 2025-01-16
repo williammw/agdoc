6ba7908d61f98ee4d3cdb76d3dc22765270a7dae
wwwwwmw <william.manwai@gmail.com>
2025-01-16 14:45:40 +0000

    ready for update

--- app/routers/twitter_router.py
+++ app/routers/twitter_router.py
@@ -15,8 +15,9 @@ router = APIRouter(tags=["twitter"])
 TWITTER_CLIENT_ID = os.getenv("TWITTER_OAUTH2_CLIENT_ID")
 TWITTER_CLIENT_SECRET = os.getenv("TWITTER_OAUTH2_CLIENT_SECRET")
 # Dynamic callback URL based on environment
-BASE_URL = os.getenv("BASE_URL")
-CALLBACK_URL = f"{BASE_URL}/twitter/callback"
+# BASE_URL = os.getenv("BASE_URL")
+# CALLBACK_URL = f"{BASE_URL}/twitter/callback"
+CALLBACK_URL = "https://dev.multivio.com/twitter/callback"
 
 def generate_code_verifier(length: int = 64) -> str:
     return secrets.token_urlsafe(length)
